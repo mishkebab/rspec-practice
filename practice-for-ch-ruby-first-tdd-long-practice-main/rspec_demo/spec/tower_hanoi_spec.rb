5b1698d83df1d7bb8f75e62be51e6012b952ca63
misha <mishabansal7@gmail.com>
2023-04-21 16:09:22 -0700

    starting towers of hanoi

---
+++ practice-for-ch-ruby-first-tdd-long-practice-main/rspec_demo/spec/tower_hanoi_spec.rb
@@ -0,0 +1,10 @@
+require "rspec_practice"
+
+describe TowerHanoi do 
+    subject(:hanoi) { TowerHanoi.new }
+
+    describe "#initialize" do
+         it "instantiates tower hanoi game with 3 towers, first one has 4 disks" do
+            expect(:hanoi).to eq
+    end 
+end 
