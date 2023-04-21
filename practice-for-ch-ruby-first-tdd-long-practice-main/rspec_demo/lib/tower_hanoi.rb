b4c6dee88366a89a7747b1e0ccb1d083d8a0bac4
misha <mishabansal7@gmail.com>
2023-04-21 16:10:56 -0700

    pushing all files

---
+++ practice-for-ch-ruby-first-tdd-long-practice-main/rspec_demo/lib/tower_hanoi.rb
@@ -0,0 +1,22 @@
+class TowerHanoi
+    # def init 
+    #     @first_tower = [4,3,2,1] ["disk", "disk", "disk", "disk"]
+    #     @second_tower = []
+    #     @third_tower = []
+            # board = [first_tower, second_tower, third_tower]
+    #     @size = @first_tower.length
+    # end 
+
+    # def user_prompt
+
+    # end
+
+    # def move
+    #     disk = (tower1).pop 
+    #     (tower.2).push(disk)
+    # end 
+    
+    # def won
+    #     @third_tower.length == @size
+    # end 
+end 
