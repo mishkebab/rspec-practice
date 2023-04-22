6629b6ae6f28776287a790094124f01f4887c14e
Matthew Montejo <montejododger@yahoo.com>
2023-04-21 17:16:20 -0700

    rspec_demo finished

--- practice-for-ch-ruby-first-tdd-long-practice-main/rspec_demo/lib/tower_hanoi.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/rspec_demo/lib/tower_hanoi.rb
@@ -1,22 +1,29 @@
 class TowerHanoi
-    # def init 
-    #     @first_tower = [4,3,2,1] ["disk", "disk", "disk", "disk"]
-    #     @second_tower = []
-    #     @third_tower = []
-            # board = [first_tower, second_tower, third_tower]
-    #     @size = @first_tower.length
-    # end 
 
-    # def user_prompt
+    attr_reader :size, :first_tower, :second_tower, :third_tower
 
-    # end
+    attr_accessor :board
+    def initialize
+        @first_tower = %w[disk disk disk disk]
+        @size = @first_tower.length
+        @second_tower = []
+        @third_tower = []
+        @board = [first_tower, second_tower, third_tower]
+    end 
 
-    # def move
-    #     disk = (tower1).pop 
-    #     (tower.2).push(disk)
-    # end 
+    def user_prompt
+        puts "enter start tower and end tower in 0'2 format"
+        arr = gets.chomp.map(&:to_i)
+    end
+
+    def move(arr)
+        start_tower, end_tower = arr
+        disk = @board[start_tower].pop 
+        @board[end_tower].push(disk)
+        @board
+    end 
     
-    # def won
-    #     @third_tower.length == @size
-    # end 
+    def won?
+        @board[2].length == @size
+    end 
 end 
